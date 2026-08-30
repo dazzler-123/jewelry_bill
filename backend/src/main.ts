eed6a55d52acbe5e044844e59d1ab12148e1385a
chandan.maurya <chandan.maurya@ebix.com>
2026-08-30 12:39:33 +0530

    son

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'http://127.0.0.1:5173'
+      'http://127.0.0.1:5173',
+      'https://frontend-five-rho-63.vercel.app'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,

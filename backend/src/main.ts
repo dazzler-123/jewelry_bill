4a5ab4ab87339b60018a865a71de0c75c88fbf66
chandan.maurya <chandan.maurya@ebix.com>
2026-08-30 12:45:05 +0530

    son

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();

85f8c4bd12b5b5912d1a641c5061c7aa0db8f6a4
chandan.maurya <chandan.maurya@ebix.com>
2026-08-30 12:00:12 +0530

    first commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,10 +7,11 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
+      'http://127.0.0.1:5173'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();

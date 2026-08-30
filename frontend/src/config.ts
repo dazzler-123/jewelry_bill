a74c43766715a87cbc8ee3e8fe9b18f1362b1833
dazzler-123 <153711536+dazzler-123@users.noreply.github.com>
2026-08-30 15:18:12 +0530

    Remove dynamic API_URL assignment

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'https://backend-chandan-mauryas-projects-241b0282.vercel.app';
+export const API_URL ='https://backend-chandan-mauryas-projects-241b0282.vercel.app';

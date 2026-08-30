5e5fc19497e3b8befa19e0999ed43e651bcebadd
chandan.maurya <chandan.maurya@ebix.com>
2026-08-30 12:47:10 +0530

    son2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,15 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://127.0.0.1:5173',
-      'https://frontend-five-rho-63.vercel.app'
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();
